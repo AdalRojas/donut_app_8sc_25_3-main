5b6e1eafb8153e8ccfd6ef5d4f95a7bd3a3b9afc
AD|ROJAS <externexion27@gmail.com>
2025-10-28 15:35:07 -0600

    Shopping cart added

--- lib/screens/home_page.dart
+++ lib/screens/home_page.dart
@@ -1,5 +1,10 @@
 import 'package:donut_app_8sc_25_3/utils/my_tab.dart';
 import 'package:flutter/material.dart';
+import '../tab/burger_tab.dart';
+import '../tab/donut_tab.dart';
+import '../tab/pancake_tab.dart';
+import '../tab/pizza_tab.dart';
+import '../tab/smoothie_tab.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({super.key});
@@ -9,13 +14,13 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
-List<Widget> myTabs = [
-  const MyTab(iconPath: 'lib/icons/donut.png', label: 'Donut'),
-  const MyTab(iconPath: 'lib/icons/burger.png', label: 'Burger'),
-  const MyTab(iconPath: 'lib/icons/pancakes.png', label: 'Pancake'),
-  const MyTab(iconPath: 'lib/icons/smoothie.png', label: 'Smoothie'),
-  const MyTab(iconPath: 'lib/icons/pizza.png', label: 'Pizza'),
-];
+  List<Widget> myTabs = [
+    const MyTab(iconPath: 'lib/icons/donut.png', label: 'Donut'),
+    const MyTab(iconPath: 'lib/icons/burger.png', label: 'Burger'),
+    const MyTab(iconPath: 'lib/icons/pancakes.png', label: 'Pancake'),
+    const MyTab(iconPath: 'lib/icons/smoothie.png', label: 'Smoothie'),
+    const MyTab(iconPath: 'lib/icons/pizza.png', label: 'Pizza'),
+  ];
   @override
   Widget build(BuildContext context) {
     return DefaultTabController(
@@ -23,54 +28,99 @@ List<Widget> myTabs = [
       child: Scaffold(
         appBar: AppBar(
           backgroundColor: Colors.transparent,
-          leading: Icon(
-            Icons.menu,
-            color: Colors.grey[800],),
-            actions: [
+          leading: Icon(Icons.menu, color: Colors.grey[800]),
+          actions: [
             Padding(
               padding: const EdgeInsets.only(right: 24.0),
-              child: Icon(
-                Icons.person,
-                color: Colors.grey[800],
+              child: Icon(Icons.person, color: Colors.grey[800]),
+            ),
+          ],
+        ),
+        body: Column(
+          children: [
+            //Texto principal
+            Padding(
+              padding: EdgeInsets.only(left: 24.0),
+              child: Row(
+                children: [
+                  Text('I want to ', style: TextStyle(fontSize: 24)),
+                  Text(
+                    'eat',
+                    style: TextStyle(
+                      fontSize: 24,
+                      fontWeight: FontWeight.bold,
+                      decoration: TextDecoration.underline,
+                    ),
+                  ),
+                ],
               ),
-            ),  
-            ],
-          ),
-          body: Column(
-            children: [
-              //Texto principal
-              Padding(
-                padding: EdgeInsets.only(left: 24.0),
-                child: Row(
-                  children: [
-                    Text('I want to ',
-                      style: TextStyle(
-                        fontSize: 24,
+            ),
+
+            //Pestañas (tabBar)
+            TabBar(tabs: myTabs),
+
+            //Contenido de pestañas (tabBarView)
+            Expanded(
+              child: TabBarView(
+                children: [
+                  Donuttab(),
+                  Burgertab(),
+                  Smoothietab(),
+                  Pancaketab(),
+                  Pizzatab(),
+                ],
+              ),
+            ),
+
+            //Carrito de compras
+            Container(
+              color: Colors.white,
+              padding: EdgeInsets.all(16),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Padding(
+                    padding: EdgeInsetsGeometry.only(left: 20),
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text(
+                          "2 Items |\$45",
+                          style: TextStyle(
+                            fontSize: 12,
+                            fontWeight: FontWeight.bold,
+                          ),
+                        ),
+                        Text(
+                          "Delivery Charges Included",
+                          style: TextStyle(fontSize: 12),
+                        ),
+                      ],
+                    ),
+                  ),
+                  ElevatedButton(
+                    style: ElevatedButton.styleFrom(
+                      backgroundColor: Colors.pink,
+                      padding: EdgeInsets.symmetric(
+                        horizontal: 24,
+                        vertical: 12,
                       ),
                     ),
-                    Text('eat',
+                    onPressed: () {},
+                    child: const Text(
+                      "View Cart",
                       style: TextStyle(
-                        fontSize: 24,
+                        color: Colors.white,
                         fontWeight: FontWeight.bold,
-                        decoration: TextDecoration.underline,
                       ),
                     ),
-                  ],
-                ),
+                  ),
+                ],
               ),
-      
-      
-              //Pestañas (tabBar)
-      
-              TabBar(tabs: myTabs),
-      
-              //Contenido de pestañas (tabBarView)
-      
-              //Carrito de compras
-            ],
-          ),
+            ),
+          ],
+        ),
       ),
     );
   }
 }
-
