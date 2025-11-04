6dee2e057ef3c2bd041a0d2934b39264b15529fb
AD|ROJAS <externexion27@gmail.com>
2025-11-04 16:08:37 -0600

    Add the other purchase options

--- lib/tab/smoothie_tab.dart
+++ lib/tab/smoothie_tab.dart
@@ -1,12 +1,59 @@
+import 'package:donut_app_8sc_25_3/utils/smoothie_tile.dart';
 import 'package:flutter/material.dart';
 
 class Smoothietab extends StatelessWidget {
-  const Smoothietab({super.key});
+  Smoothietab({super.key});
+
+  //list of donuts
+  final List donutOnsale = [
+    //donutFlavor, donutPrice,donut,color
+    [
+      'Chocolate',
+      '100',
+      Colors.brown,
+      "lib/images/chocolate_donut.png",
+      'Dunkin Donot',
+    ],
+    ['Grape', '80', Colors.purple, "lib/images/grape_donut.png", 'oxxo'],
+    [
+      'Ice cream',
+      '95',
+      Colors.blue,
+      "lib/images/icecream_donut.png",
+      'Staebusc',
+    ],
+    [
+      'Strawberry',
+      '89',
+      Colors.red,
+      "lib/images/strawberry_donut.png",
+      'Krispi',
+    ],
+  ];
 
   @override
   Widget build(BuildContext context) {
-    return const Center(
-      child: Text('Smoothietab Tab Content'),
+    return GridView.builder(
+      //Se encarga de acomodar
+      gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+        crossAxisCount: 2,
+        childAspectRatio: 1 / 1.4,
+      ),
+
+      //Cuantos elemento tendra
+      itemCount: donutOnsale.length,
+
+      itemBuilder: (context, index) {
+        return SmoothieTile(
+          donutFlavor: donutOnsale[index][0],
+          donutPrice: donutOnsale[index][1],
+          donutColor: donutOnsale[index][2],
+          donutImagePaht: donutOnsale[index][3],
+          donutProvider: donutOnsale[index][4],
+        );
+      },
+
+      //loque se va a construir en el drig
     );
   }
 }
